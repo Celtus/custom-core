2951df078e3641a479cf5c36dfeee92a5e563bc4
Celtus <celtus614@gmail.com>
2014-09-07 09:50:10 -0400

    0001-12714-Unify-and-beatuify-system-startup-text.patch Failed! - patching file src/game/World.cpp Hunk #22 FAILED at 1421.

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -4397,6 +4397,7 @@ void Player::DeleteOldCharacters(uint32 keepDays)
         while (resultChars->NextRow());
         delete resultChars;
     }
+    sLog.outString();
 }
 
 void Player::SetRoot(bool enable)
